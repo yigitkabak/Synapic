e38186e8f883098efa7daab052a7bae781a011b5
Yiğit KABAK <yigitkabak@proton.me>
2025-03-16 14:21:25 +0300

    Delete synapic/cli.js

--- synapic/cli.js
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env node
-const path = require('path');
-const { spawn } = require('child_process');
-
-const appPath = path.join(__dirname, 'app.js');
-spawn('node', [appPath], { cwd: __dirname, stdio: 'inherit' });
